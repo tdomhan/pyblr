328428a2440d02fe2339204c48591aefbfcd54b1
Tobias Domhan <tobias@bit.ly>
2013-06-14 17:46:49 -0400

    better parameter checking

--- src/blr.py
+++ src/blr.py
@@ -1,9 +1,7 @@
 '''
-Created on May 1, 2013
-
-@author: tdomhan
-
 Baysian linear regression.
+
+For usage see test.py
 
 Depends on numpy (and optionally matplotlib for plotting).
 
@@ -24,16 +22,21 @@ class BayesianLinearRegression:
 
     P(y|x,beta) ~ N(y|x.dot(beta),sigma**2)
   """
-  def __init__(self, lamb=[20.,20.], beta_mu=[0.,0.], sigma=5, fit_intercept=True):
+  def __init__(self, lamb=20., beta_mu=0, sigma=5, fit_intercept=True):
     """
       lamb: variance of the prior for each of the feature dimensions.
       beta_mu: the mean of the prior
       sigma: variance of the prediction error.
 
-      TODO: don't expect the data dimension to be 2 for the default args
     """
-    self.inv_lamb = 1./np.asarray(lamb)
-    self.beta_mu  = np.asarray(beta_mu)
+    if not np.isscalar(lamb):
+      self.inv_lamb = 1./np.asarray(lamb)
+    else:
+      self.inv_lamb = 1./float(lamb)
+    if not np.isscalar(beta_mu):
+      self.beta_mu  = np.asarray(beta_mu)
+    else:
+      self.beta_mu = float(beta_mu)
 
     self.sigma = sigma
     self.fit_intercept = fit_intercept
@@ -44,7 +47,12 @@ class BayesianLinearRegression:
     return X_new
 
   def fit_ml(self, X, y):
-    "Fit a Maximum Likelihood estimate. (not Bayesian)"
+    """
+      Fit a Maximum Likelihood estimate. (not Bayesian)
+
+      X: features, n_samples by n_features nd-array
+      y: target values, n_samples array
+    """
     if self.fit_intercept:
       X = self.add_intercept(X)
     self.beta = LA.inv(X.T.dot(X)).dot(X.T.dot(y))
@@ -52,26 +60,37 @@ class BayesianLinearRegression:
   def fit_map(self, X, y, full_posterior=True):
     """
       Fit a MAP estimate
+
+      X: features, n_samples by n_features nd-array
+      y: target values, n_samples array
     """
     if self.fit_intercept:
       X = self.add_intercept(X)
+    #data setup
+    f_dim = X.shape[1]
+    if np.isscalar(self.inv_lamb):
+      inv_lamb = np.diagflat(np.repeat(self.inv_lamb,f_dim))
+    else:
+      inv_lamb = np.diagflat(self.inv_lamb)
+    if np.isscalar(self.beta_mu):
+      beta_mu = np.repeat(self.beta_mu,f_dim)
+    else:
+      beta_mu = self.beta_mu
     sigma = self.sigma
-    l = sigma**2 * np.diagflat(self.inv_lamb)
+    #let the actual calculation begin
+    l = sigma**2 * inv_lamb
     s = LA.inv(X.T.dot(X)+l)
-    b0 = sigma**2 * np.diagflat(self.inv_lamb).dot(self.beta_mu)
+    # adding in the mean of the prior
+    b0 = sigma**2 * inv_lamb.dot(beta_mu)
 
     self.beta = s.dot(X.T.dot(y) + b0)
     if full_posterior:
       self.Sigma = sigma * sigma * s
 
-
   def predict(self, X):
     if self.fit_intercept:
       X = self.add_intercept(X)
     return X.dot(self.beta)
-
-
-
 
 
 
--- src/test.py
+++ src/test.py
@@ -5,6 +5,11 @@
 import blr
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib import cm
+from matplotlib.colors import LogNorm
+
 def mse(X,y,clf):
     d = y-clf.predict(X)
     return 1/float(len(y)) * d.T.dot(d)
@@ -18,22 +23,41 @@ y_train = np.loadtxt("../test-data/Ytrain.txt")
 
 n_samples = [2,10,100]
 
-#clf = blr.BayesianLinearRegression(lamb=np.array([20.,20.]), sigma=5, fit_intercept=False)
+def run_clf(clf):
+  for n in n_samples:
+      X = Xtrain[:n]
+      y = y_train[:n]
+      clf.fit_map(X,y)
+      error_map = mse(Xtest,y_test,clf)
+      clf.fit_ml(X,y)
+      error_ml = mse(Xtest,y_test,clf)
+
+      print "samples: %d, ML mse: %f MAP mse: %f" % (n,error_ml, error_map)
+
+
+# test different initializations of the classifier:
+
+clf = blr.BayesianLinearRegression(fit_intercept=False)
+print "without arguments"
+run_clf(clf)
+
+print "vector arguments"
+run_clf(clf)
 clf = blr.BayesianLinearRegression(
         lamb=np.array([20.,20.]),
         sigma=5, fit_intercept=False,
         beta_mu=[0,0])
 
+print "scalar arguments"
+run_clf(clf)
+clf = blr.BayesianLinearRegression(
+        lamb=20,
+        sigma=5, fit_intercept=False,
+        beta_mu=0)
 
-for n in n_samples:
-    X = Xtrain[:n]
-    y = y_train[:n]
-    clf.fit_map(X,y)
-    error_map = mse(Xtest,y_test,clf)
-    clf.fit_ml(X,y)
-    error_ml = mse(Xtest,y_test,clf)
+#let's do some interesting plots:
 
-    print "samples: %d, ML mse: %f MAP mse: %f" % (n,error_ml, error_map)
+
 
 
 
